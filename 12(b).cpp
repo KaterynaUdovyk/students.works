23d2f17074552d8cbba70024cbd8aa52fe175e16
KaterynaUdovyk <78071191+KaterynaUdovyk@users.noreply.github.com>
2021-02-02 20:33:07 +0200

    hw 01(5),02(9),12(b)

---
+++ 12(b).cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+double a,b,c,d,e;
+int main() {
+
+    printf("x|1|2|3|4|5 \n");
+    printf("___+___+___+___+___+___\n");
+    a=pow(1,2);
+    b=pow(2,2);
+    c=pow(3,2);
+    d=pow(4,2);
+    e=pow(5,2);
+    printf("F(x)|%.2lf|%.2lf|%.2lf|%.2lf|%.2lf\n",a,b,c,d,e);
+
+
+    return 0;
+}
